27937321f0baf11a2013be14bdcbfbce37f2a99a
Stefan <szdralje@gmail.com>
2026-05-06 16:20:39 -0700

    added manual test space and files

---
+++ manual_test_space/helper_script.R
@@ -0,0 +1,36 @@
+devtools::install_github("Thatguy027/goldengateR", force = TRUE)
+library(goldengateR)
+
+
+setwd(glue::glue("{dirname(rstudioapi::getActiveDocumentContext()$path)}/"))
+
+set_plasmid_dir("/Users/Stefan/UCLA/Projects/synthetic_yeast/project_plan/GG_Rpackage/xylose_parts/")
+
+assemble_interactive(enzyme = "BsaI", output_dir = "./plasmids")  
+
+
+entry_vec <- load_plasmid("pYTK033")
+backbone  <- digest_and_select(entry_vec, "BsaI", select = "largest")
+
+# 2. PCR-amplify your gene with BsaI primer tails, digest, take the insert
+amp    <- read_part("pcr_products/xylb_pcr.fa", topology = "linear")
+insert <- digest_and_select(amp, "BsaI", select = "largest")
+
+# 3. Ligate
+asm <- ligate(list(backbone, insert),
+              enzyme = "BsaI",
+              name   = "xylb_part",
+              output = "xylose_parts/xylb_part.gb")
+
+# 2. PCR-amplify your gene with BsaI primer tails, digest, take the insert
+amp    <- read_part("pcr_products/gal2mut_pcr.fa", topology = "linear")
+insert <- digest_and_select(amp, "BsaI", select = "largest")
+
+# 3. Ligate
+asm <- ligate(list(backbone, insert),
+              enzyme = "BsaI",
+              name   = "gal2_part",
+              output = "xylose_parts/gal2_part.gb")
+
+
+results <- assemble_from_table("goldengateR/assemblies_example.tsv", output_dir = "xylose_parts")
